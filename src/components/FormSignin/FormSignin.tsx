56ef96028b788d3cf0792ffe14dd4771a6420c07
Dayan <dayaara6@gmail.com>
2023-05-29 22:22:50 -0400

    sign up and sign in

---
+++ src/components/FormSignin/FormSignin.tsx
@@ -0,0 +1,71 @@
+
+"use client"
+import signIn from '@/firebase/auth/signin';
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+const FormSignin = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleForm = async (event: any) => {
+    event.preventDefault();
+
+    const { result, error } = await signIn(email, password);
+
+    if (error) {
+      return console.log(error);
+    }
+
+    // else successful
+    console.log(result);
+  }
+  // return router.push("/admin");
+  return (
+
+    <div className='flex justify-center gap-5'>
+      <div>
+        <form onSubmit={handleForm} className="form">
+          <div className="mb-4">
+            <label htmlFor="email" className="block">
+              <span>Email</span>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                type="email"
+                name="email"
+                id="email"
+                placeholder="example@mail.com"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block">
+              <span>Password</span>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <button type="submit" className="bg-indigo-500 mb-5  text-white py-2 px-4 rounded">
+            Sign In
+          </button>
+        </form>
+        <Link href='/company/signup' className="text-indigo-500  underline">Crear una cuenta</Link>
+      </div>
+
+    </div>
+  );
+
+}
+export default FormSignin
---
+++ src/components/FormSignup/FormSignup.tsx
@@ -0,0 +1,79 @@
+"use client"
+import signUp from '@/firebase/auth/signup';
+import addData from '@/firebase/firestore/addData';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+
+const FormSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nombre, setNombre] = useState("");
+  const router = useRouter();
+
+  const handleForm = async (event: any) => {
+    event.preventDefault();
+
+    const { result, error } = await signUp(email, password);
+    const data = {
+      nombre,
+      correo: result?.user.email
+    }
+    const { result: addDataResult, error: addDataError } = await addData("empresas", result?.user.uid, data);
+    if (error || addDataError) {
+      return console.log(error);
+    }
+
+    console.log(result);
+
+    return router.push("/company/ofertas");
+  };
+  return (
+    <div className='flex justify-center gap-5'>
+      <form  className="form">
+        <label htmlFor="email">
+          <p>Email</p>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="email"
+            name="email"
+            id="email"
+            placeholder="example@mail.com"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label>
+          <p>Nombre de la Empresa</p>
+          <input
+            onChange={(e) => setNombre(e.target.value)}
+            required
+            type="text"
+            name="nombre"
+            value={nombre}
+            id="nombre"
+            placeholder="Infojobs"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <label htmlFor="password">
+          <p>Password</p>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+        <button onClick={handleForm} type="submit" className="bg-indigo-500 mt-5  text-white py-2 px-4 rounded">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default FormSignup
