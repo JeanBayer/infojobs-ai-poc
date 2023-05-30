36aa1a41ffc29fb667d0af8d79e77e5501cec04d
Dayan <dayaara6@gmail.com>
2023-05-30 15:11:11 -0400

    modificiacion de la homepage

--- src/components/CardCandidato/CardCandidato.tsx
+++ src/components/CardCandidato/CardCandidato.tsx
@@ -13,7 +13,7 @@ export const CardCandidato = ({ name, probabilidad, rol }: CardCandidato) => {
       <div className="card-body">
         <div className="flex justify-between">
           <h2 className="card-title">{name}</h2>
-          <span className="">{probabilidad}</span>
+          <span className="">{probabilidad}%</span>
         </div>
         <p className="text-xs">{rol}</p>
         <div className="divider"></div>
